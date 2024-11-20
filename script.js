2b13fec2a4b767600af06b50de3962130795cc7f
jaswanthchowdary99 <154533631+jaswanthchowdary99@users.noreply.github.com>
2024-11-20 15:11:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let removeColor = document.querySelector('input[type="button"]')
+removeColor.addEventListener('click',function(){
+	let colorSelect = document.getElementById('colorSelect');
+	let colorIndex = colorSelect.colorIndex;
+	if(colorIndex !== -1){
+		colorSelect.remove(colorIndex);
+	}
+})
